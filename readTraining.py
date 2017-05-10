506f8f1395fec7dfead4a69a9e5b213f03a30542
grgp <g.a.pitoy@gmail.com>
2017-05-10 19:38:21 +0700

    Tiny

--- readTraining.py
+++ readTraining.py
@@ -1,28 +1,46 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+def postgd(line):
+    return word_tokenize(line.strip())
+
+def joinBackTogether(words):
+    s = ""
+    for word in words:
+        if len(word) == 1 and word[0] in [',', '.', '!', '?', ':', ';']:
+            s += word
+        else:
+            s += ' ' + word
+    return s.strip()
+
+def toNERTaggedTuples(line):
+    taggedWords = []
+    taggedRaw = line.split('<ENAMEX TYPE="')
+    if len(taggedRaw[0]) > 0:
+        residue = taggedRaw[0].strip().split(' ')
+        taggedWords.extend([(word, 'X') for word in residue])
+    for fragment in taggedRaw[1:]:
+        residue = fragment.split('</ENAMEX>')
+        th = residue[0].strip().split('">')
+        taggedWords.append((th[1], th[0]))
+        
+        filtered = [(word, 'X') for word in residue[1].split(' ') if len(word) > 0]
+        taggedWords.extend(filtered)
+
+    return taggedWords
+
 with open("training_data_new.txt") as f:
     errorCount = 0
     for idx, line in enumerate(f):
         try:
             if idx == 456:
-                print(line)
-                taggedWords = []
-                taggedRaw = line.split('<ENAMEX TYPE="')
-                if len(taggedRaw[0]) > 0:
-                    residue = taggedRaw[0].strip().split(' ')
-                    taggedWords.extend([(word, 'X') for word in residue])
+                taggedWords = toNERTaggedTuples(line)
+                justWords = [tuple[0] for tuple in taggedWords]
+
                 print(taggedWords)
-                for fragment in taggedRaw[1:]:
-                    residue = fragment.split('</ENAMEX>')
-                    th = residue[0].strip().split('">')
-                    taggedWords.append((th[1], th[0]))
-                    
-                    filtered = [(word, 'X') for word in residue[1].split(' ') if len(word) > 0]
-                    taggedWords.extend(filtered)
-                print(taggedWords)
+                print(joinBackTogether(justWords))
         except UnicodeEncodeError:
-            print("Error yo")
+            print("Error yo, I'm counting dis.")
             errorCount += 1
 
     print("errorCount: " + str(errorCount))
-
-# s = 'Batu <E="Org">FBI</E> adalah <E="Prs">Bro</E> kapas.'
-# print(str(s.split('<E="')))
